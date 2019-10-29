b32dce3f899d5fcfeb755b3907ed86b4a9194ef4
chanukand <chanukaadasanayaka1@gmail.com>
2019-10-29 17:24:50 +0530

    lll

--- pointOfSale/src/possystem/view/category.java
+++ pointOfSale/src/possystem/view/category.java
@@ -249,7 +249,7 @@ public class category extends JInternalFrame {
         obj.setVisible(true);
     }//GEN-LAST:event_btnAddActionPerformed
     private void newForm(){
-        System.out.println("tytytyt");
+        System.out.println("tytytytjkjk");
     }
     /**
      * @param args the command line arguments
