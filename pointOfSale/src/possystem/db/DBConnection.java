bcf68ef3e5a69ca021279c33627635aa8b2ddc75
Chanuka Dasanayaka <chanukad@fortude.co>
2019-10-29 20:48:38 +0530

    db connection and jar file update

--- pointOfSale/src/possystem/db/DBConnection.java
+++ pointOfSale/src/possystem/db/DBConnection.java
@@ -14,7 +14,7 @@ public class DBConnection {
 
     private DBConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        con = DriverManager.getConnection("jdbc:mysql://204.93.168.209:3306/chanukan_microfinance?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "chanukand", "nokia5130");
+        con = DriverManager.getConnection("jdbc:mysql://204.93.168.209:3306/chanukan_dadspos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "chanukand", "nokia5130");
         //con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dadspos", "root", "1234");
     }
 
